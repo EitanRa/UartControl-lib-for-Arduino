cf29ab709cdf6e327ef8adc646dcf5897365d5f0
EitanRa <34000846+EitanRa@users.noreply.github.com>
2020-08-03 21:20:18 +0300

    Add files via upload

---
+++ UartControl_Master/UartControl_ArduinoLib/README.txt
@@ -0,0 +1,20 @@
+UartControl by Eitan Rabinovitch
+
+Save this folder in: Arduino\libraries
+
+Commands:
+begin - Start simulating
+click - Mouse-left-button click
+rightClick - Mouse-right-button click
+leftDown - Mouse-left-button down
+leftUp - Mouse-left-button up
+rightDown - Mouse-right-button down
+rightUp - Mouse-right-button up
+middleDown - Mouse-middle-button down
+middleUp - Mouse-middle-button up
+moveCursor(int xsteps, int ysteps) - Move the cursor x and y steps
+getCursorX - Get cursor's x value[-1 = Error]
+getCursorY - Get Cursor's y value[-1 = Error]
+keyPress(String key) - Key press[List of special keys here: https://katzr.net/8ffd28]
+
+http://www.eitanra.022.co.il/BRPortal/br/P100.jsp
---
+++ UartControl_Master/UartControl_ArduinoLib/UartControl.cpp
@@ -0,0 +1,87 @@
+/*
+  UartControl.cpp - Library for controlling mouse and keyboard input in Windows computers using Uart communication.
+  Created by Eitan Rabinovitch, July 14, 2020.
+  Released into the public domain.
+  http://www.eitanra.022.co.il/BRPortal/br/P100.jsp
+*/
+#include "Arduino.h"
+#include "UartControl.h"
+
+UartControl::UartControl(int baudRate) {
+    _br = baudRate;
+}
+
+void UartControl::begin() {
+    Serial.begin(_br);
+}
+
+void UartControl::click() {
+    Serial.println("m ld");
+    Serial.println("m lu");
+}
+
+void UartControl::rightClick() {
+    Serial.println("m rd");
+    Serial.println("m ru");
+}
+
+void UartControl::leftDown() {
+    Serial.println("m ld");
+}
+
+void UartControl::leftUp() {
+    Serial.println("m lu");
+}
+
+void UartControl::rightDown() {
+    Serial.println("m rd");
+}
+
+void UartControl::rightUp() {
+    Serial.println("m ru");
+}
+
+void UartControl::middleDown() {
+    Serial.println("m md");
+}
+
+void UartControl::middleUp() {
+    Serial.println("m mu");
+}
+
+void UartControl::moveCursor(int xsteps, int ysteps) {
+    if(xsteps > -1) {
+        Serial.println("x+" + String(xsteps));
+    }
+    else {
+        Serial.println("x-" + String(xsteps*-1));
+    }
+    if(ysteps > -1) {
+        Serial.println("y+" + String(ysteps));
+    }
+    else {
+        Serial.println("y-" + String(ysteps*-1));
+    }
+}
+
+int UartControl::getCursorX() {
+    Serial.println("gx");
+    int x = -1;
+    if(Serial.available() > 0) {
+        x = Serial.parseInt();
+    }
+    return x;
+}
+
+int UartControl::getCursorY() {
+    Serial.println("gy");
+    int y = -1;
+    if(Serial.available() > 0) {
+        y = Serial.parseInt();
+    }
+    return y;
+}
+
+void UartControl::keyPress(String key) {
+    Serial.println("k " + key);
+}
