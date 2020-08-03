c82437479619ac8c151db726ac273656d99d5d87
EitanRa <34000846+EitanRa@users.noreply.github.com>
2020-08-03 21:29:59 +0300

    Update UartControl.cpp

--- UartControl_Master/UartControl_ArduinoLib/UartControl.cpp
+++ UartControl_Master/UartControl_ArduinoLib/UartControl.cpp
@@ -1,5 +1,5 @@
 /*
-  UartControl.cpp - Library for controlling mouse and keyboard input in Windows computers using Uart communication.
+  UartControl.cpp - Library for controlling mouse and keyboard in Windows computers using Uart communication.
   Created by Eitan Rabinovitch, July 14, 2020.
   Released into the public domain.
   http://www.eitanra.022.co.il/BRPortal/br/P100.jsp
